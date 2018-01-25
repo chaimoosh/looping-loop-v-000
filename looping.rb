ce309731b56ddddcd925d4a52b131d1a28fd03ca
Chaim Reisman <crreisman@gmail.com>
2018-01-25 22:36:05 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,14 @@
-def looping
+#def looping
 	#your code here
+	counter = 0 
+	loop do
+	  counter += 1 
+	  puts "No #{counter}"
+	  if counter > 15
+	    break
+	  end
+	  
 end
 
 #call your method here
+looping
